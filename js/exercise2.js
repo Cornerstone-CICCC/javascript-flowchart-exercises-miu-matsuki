97da3ae74a5e3a4a1dbc51e3ff51c9dece2924a8
miu-matsuki <miu.m.works@gmail.com>
2026-05-20 12:20:37 -0700

    add exercise2.js

---
+++ js/exercise2.js
@@ -0,0 +1,18 @@
+//https://github.com/Cornerstone-CICCC/javascript-flowchart-exercises-miu-matsuki
+
+
+// Write an algorithm and draw a flowchart that will read the two sides of a rectangle and calculate its area.
+
+// Pseudocode:
+
+// Input the width (W) and the length (L) of a rectangle
+// Calculate the area (A) by multiplying W by L
+// Print A
+console.log("This is exercise2");
+
+let wid = 5;
+let len = 7;
+
+let area =  wid * len;
+
+console.log(area);
